c0074be3d4c47e89130e9520df4c7450fcbdf894
nklhrstv <me@nikolahristov.com>
2020-04-05 14:52:35 +0300

    inline tags removed from encoder functions

--- src/deep_links/encoder.rs
+++ src/deep_links/encoder.rs
@@ -1,12 +1,10 @@
 use percent_encoding::{utf8_percent_encode, NON_ALPHANUMERIC};
 use stremio_core::types::addons::ExtraProp;
 
-#[inline]
 pub fn encode_uri_component(component: &str) -> String {
     utf8_percent_encode(component, NON_ALPHANUMERIC).to_string()
 }
 
-#[inline]
 pub fn encode_query_params(query_params: &[ExtraProp]) -> String {
     query_params
         .iter()
